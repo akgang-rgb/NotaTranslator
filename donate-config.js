6a2369874e4096404bba07d8447752df71174268
akgang-rgb <akgang@gmail.com>
2026-09-07 13:36:24 +0200

    stripe finalizing

--- donate-config.js
+++ donate-config.js
@@ -1,3 +1,3 @@
 // Public configuration only. Set this to the deployed donation service URL.
 // Never put a Stripe secret key in this file or anywhere in the website.
-window.NOTATR_DONATIONS = Object.freeze({ apiBase: '' });
+window.NOTATR_DONATIONS = Object.freeze({ apiBase: 'https://getexp.akgang.workers.dev' });
